fdedf0fc2a36020c97fe2380ce73f7ade2308940
hamdi458 <hamdi.645@gmail.com>
2020-09-28 17:03:37 +0100

    task4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    let amn = {};
+  
+    $(document).on('change', "input[type='checkbox']", function () {
+        
+        if (this.checked) {
+            amn[$(this).data('id')] = $(this).data('name');
+          }
+          else if($(this).is(':not(:checked)'))
+          {
+            delete amn[$(this).data('id')];
+          }
+        $('div.amenities > h4').text(Object.values(amn).join(', '));
+        });
+  });
+  $(document).ready(function(){
+    $.getJson('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+        if (textStatus === 'success' && data.status === 'OK') {
+        
+            $('#api_status').addClass('available'); }
+         else {
+            $('#api_status').removeClass('available');
+        }
+        
+    });
+    $.ajax({
+        type: 'POST',
+        url: "http://0.0.0.0:5001/api/v1/places_search/",
+        data: {},
+        contentType: 'application/json',
+        success: function (data) {
+            $.each(response1, function (i, item) {
+                var line = '<article><h2>' + item.name + 
+                '</h2><div class="price_by_night"><p>$' + item.price_by_night + 
+                '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + item.max_guest + 
+                '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + item.number_rooms + 
+                '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + item.number_bathrooms + 
+                '</p></div></div><div class="description"><p>' + item.description + 
+                '</p></div></article>';
+                $("#places").append(line);
+
+            });
+        }
+    });
+}); 
