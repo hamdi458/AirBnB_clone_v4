946fb27f505f4a78697e850be7577273b65bdfe3
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-27 22:35:59 -0400

    checkbox

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    let amn = {};
+  let amn = {};
 
-    $(document).on('change', "input[type='checkbox']", function () {
-        
-        if (this.checked) {
-            checkedAmenities[$(this).data('id')] = $(this).data('name');
-          }
-          else if($(this).is(':not(:checked)'))
-          {
-            delete amn[$(this).data('id')];
-          }
-        $('div.amenities > h4').text(Object.values(amn).join(', '));
-        });
+  $(document).on('change', "input[type='checkbox']", function () {
+      
+      if (this.checked) {
+          amn[$(this).data('id')] = $(this).data('name');
+        }
+        else if($(this).is(':not(:checked)'))
+        {
+          delete amn[$(this).data('id')];
+        }
+      $('div.amenities > h4').text(Object.values(amn).join(', '));
+      });
 });
